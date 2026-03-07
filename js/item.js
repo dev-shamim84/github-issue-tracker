29e8caff71f3f8592df3e9bdd4737449a93cc642
Md Shamim Hasan <mdshamimhasan791@gmail.com>
2026-03-07 16:10:12 +0600

    customize card funcetionality

---
+++ js/item.js
@@ -0,0 +1,6 @@
+${
+                item.priority === "high"
+                  ? `class="bg-[#FEECEC]  rounded-full font-bold uppercase py-2 px-4 text-[#EF4444]"`
+                  : item.priority === "low"
+                  ? `class="bg-[#FEECEC]  rounded-full font-bold uppercase py-2 px-4 text-[#EF4444]`
+                  : ""
