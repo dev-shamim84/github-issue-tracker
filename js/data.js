33b5810e989669fc78037f18888a52991a036663
Md Shamim Hasan <mdshamimhasan791@gmail.com>
2026-03-07 10:37:16 +0600

    show card in display dynamically function inplement

--- js/data.js
+++ js/data.js
@@ -48,44 +48,44 @@ closeBtn.addEventListener("click", () => {
 });
 
 // display data
-// const displayData = (data) => {
-//   const cardContainer = document.getElementById("card-container");
-//   cardContainer.innerHTML = "";
-//   data.map((item) => {
-//     const card = document.createElement("div");
-//     card.innerHTML = `
-//      <div ${
-//        item.status === "open"
-//          ? `class="border-t-4 border-[#00A96E] px-6 py-4 shadow space-y-4 h-[340px] bg-white rounded"`
-//          : `class="border-t-4 border-[#A855F7] px-6 py-4 shadow space-y-4 h-[340px] bg-white rounded"`
-//      } >
-//           <div class="flex justify-between items-center">
-//             <div><img src="./assets/Open-Status.png" alt="Open-Status.png"></div>
-//             <div class="bg-[#FEECEC]  rounded-full">
-//               <h5 class="font-bold uppercase py-2 px-4 text-[#EF4444]">${
-//                 item.priority
-//               }</h5>
-//             </div>
-//           </div>
-//           <h4 class="font-semibold">Fix ${item.title}</h4>
-//           <p class="line-clamp-2 font-[12px] text-[#647488]">${
-//             item.description
-//           }</p>
-//           <div>
-//             <span class="bg-[#FECACA] py-1 px-2 rounded-full text-[#EF4444]"><i class="fa-solid fa-bug"></i> Bug</span>
-//             <span class="bg-[#FECACA] py-1 px-2 rounded-full text-[#EF4444]"><i class="fa-regular fa-life-ring"></i>
-//               help wanted</span>
-//           </div>
-//           <hr class="opacity-15">
-//           <div>
-//             <p class="font-[12px] text-[#647488]">${item.author}</p>
-//             <p class="font-[12px] text-[#647488]">${item.createdAt}</p>
-//           </div>
-//         </div>
+const displayData = (data) => {
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  data.map((item) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+     <div ${
+       item.status === "open"
+         ? `class="border-t-4 border-[#00A96E] px-6 py-4 shadow space-y-4 h-[340px] bg-white rounded"`
+         : `class="border-t-4 border-[#A855F7] px-6 py-4 shadow space-y-4 h-[340px] bg-white rounded"`
+     } >
+          <div class="flex justify-between items-center">
+            <div><img src="./assets/Open-Status.png" alt="Open-Status.png"></div>
+            <div class="bg-[#FEECEC]  rounded-full">
+              <h5 class="font-bold uppercase py-2 px-4 text-[#EF4444]">${
+                item.priority
+              }</h5>
+            </div>
+          </div>
+          <h4 class="font-semibold">Fix ${item.title}</h4>
+          <p class="line-clamp-2 font-[12px] text-[#647488]">${
+            item.description
+          }</p>
+          <div>
+            <span class="bg-[#FECACA] py-1 px-2 rounded-full text-[#EF4444]"><i class="fa-solid fa-bug"></i> Bug</span>
+            <span class="bg-[#FECACA] py-1 px-2 rounded-full text-[#EF4444]"><i class="fa-regular fa-life-ring"></i>
+              help wanted</span>
+          </div>
+          <hr class="opacity-15">
+          <div>
+            <p class="font-[12px] text-[#647488]">${item.author}</p>
+            <p class="font-[12px] text-[#647488]">${item.createdAt}</p>
+          </div>
+        </div>
 
-//     `;
-//     cardContainer.appendChild(card);
-//   });
-// };
+    `;
+    cardContainer.appendChild(card);
+  });
+};
 
 loadData();
